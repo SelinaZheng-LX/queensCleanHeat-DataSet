1e94f87169655e75ae7f4036aa2481cc4e83bd57
SelinaZheng <selinazhenglx@gmail.com>
2023-10-20 16:00:58 -0400

    Setup
    import and started to clean data

--- analysingData.py
+++ analysingData.py
@@ -1,1 +1,13 @@
 #Team Queens
+import pandas as pd
+import matplotlib.pyplot as plt
+import gdown
+
+clean_heat = pd.read_csv("queensCleanHeat-DataSet/clean_heat_dataset.csv")
+#extract stats for a particular categorical column
+cat_stats = clean_heat.describe(include=['O'])
+print("Borough:\n", cat_stats['Borough'])
+
+#Cleaning the data
+#check if any values are null
+print("Number of dataponts with null entry for each column:\n",clean_heat.isnull().sum())
